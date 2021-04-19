353547b16467e3408b69fdcf8b690a57e62a21c8
Yusaku Matsuki <ymatzki@gmail.com>
2021-04-19 10:28:18 +0900

    feat: show flag

--- src/App.tsx
+++ src/App.tsx
@@ -3,60 +3,18 @@ import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { Speak } from "./modules/Speak";
+import japan from "./images/a23.gif";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p
-          onClick={() => {
-            Speak("save to reload.");
-          }}
-        >
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <input
+      type="image"
+      id="button"
+      src={japan}
+      onClick={() => {
+        Speak("Japan");
+      }}
+    ></input>
   );
 }
 
