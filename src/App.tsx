cbfe407638329cd3cfe05b47ef3e56778bb7ab77
Yusaku Matsuki <ymatzki@gmail.com>
2021-04-23 05:40:08 +0900

    feat: apply style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import UIkit from "uikit";
+import "uikit/dist/css/uikit.min.css";
+
 import "./App.css";
 import { Speak } from "./modules/Speak";
 import Countries from "./data/ja_countries.json";
@@ -16,21 +17,31 @@ function importAll(r: __WebpackModuleApi.RequireContext) {
 function App() {
   const images = importAll(require.context("./images", false, /\.(gif)$/));
   return (
-    <>
+    <ul
+      className="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s"
+      data-uk-sortable="handle: .uk-card"
+      data-uk-grid
+    >
       {Countries.map((value) => {
         return (
-          <input
-            key={value.images}
-            type="image"
-            id="button"
-            src={images.get("./" + value.images)}
-            onClick={() => {
-              Speak(value.name, "ja");
-            }}
-          ></input>
+          <li>
+            <div
+              className="uk-card uk-card-default uk-card-body uk-text-center"
+              key={value.images}
+            >
+              <input
+                type="image"
+                id="button"
+                src={images.get("./" + value.images)}
+                onClick={() => {
+                  Speak(value.name, "ja");
+                }}
+              ></input>
+            </div>
+          </li>
         );
       })}
-    </>
+    </ul>
   );
 }
 
