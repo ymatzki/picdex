13efdb5655a5660a7ccb571c83ad23ac0ac1cbb7
Yusaku Matsuki <ymatzki@gmail.com>
2021-04-24 17:48:52 +0900

    feat: add header

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -5,7 +5,10 @@ from bs4 import BeautifulSoup
 
 
 base_url = 'https://www.mofa.go.jp/mofaj/kids/kokki/'
-url = base_url + 'k_asia.html'
+# url = base_url + 'k_asia.html'
+# url = base_url + 'k_chuto.html'
+# url = base_url + 'k_n_america.html'
+url = base_url + 'k_latinamerica.html'
 soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
 countries = []
@@ -14,7 +17,7 @@ for element in soup.find_all("li", class_="heightLine"):
     img = element.find('img')['src'].split("/")[1]
     name = element.find('p').text
     countries.append({"images": img, "name": name})
-    urllib.request.urlretrieve(base_url+"image/"+img, "images"+img)
+    urllib.request.urlretrieve(base_url+"image/"+img, img)
 
 with open("ja_countries.json", "w") as f:
     f.write(str(countries))
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import UIkit from "uikit";
+import "uikit";
 import "uikit/dist/css/uikit.min.css";
 
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { Speak } from "./modules/Speak";
 import Countries from "./data/ja_countries.json";
@@ -17,14 +19,15 @@ function importAll(r: __WebpackModuleApi.RequireContext) {
 function App() {
   const images = importAll(require.context("./images", false, /\.(gif)$/));
   return (
-    <ul
-      className="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s"
-      data-uk-sortable="handle: .uk-card"
-      data-uk-grid
-    >
-      {Countries.map((value) => {
-        return (
-          <li>
+    <>
+      <h1 className="uk-heading-divider">picdex</h1>
+      <ul
+        className="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s"
+        data-uk-sortable="handle: .uk-card"
+        data-uk-grid
+      >
+        {Countries.map((value) => {
+          return (
             <div
               className="uk-card uk-card-default uk-card-body uk-text-center"
               key={value.images}
@@ -38,10 +41,10 @@ function App() {
                 }}
               ></input>
             </div>
-          </li>
-        );
-      })}
-    </ul>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
