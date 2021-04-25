5f0ba232199ef01c25cb1ae283901a2b0c9b084c
Yusaku Matsuki <ymatzki@gmail.com>
2021-04-26 08:10:48 +0900

    feat: add countries

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -8,15 +8,22 @@ base_url = 'https://www.mofa.go.jp/mofaj/kids/kokki/'
 # url = base_url + 'k_asia.html'
 # url = base_url + 'k_chuto.html'
 # url = base_url + 'k_n_america.html'
-url = base_url + 'k_latinamerica.html'
+# url = base_url + 'k_latinamerica.html'
+# url = base_url + 'k_europe.html's
+# url = base_url + 'k_oceania.html'
+url = base_url + 'k_africa.html'
+
 soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
 countries = []
 
 for element in soup.find_all("li", class_="heightLine"):
     img = element.find('img')['src'].split("/")[1]
-    name = element.find('p').text
-    countries.append({"images": img, "name": name})
+    name = element.find('p')
+    if name is None :
+        name = element.find('span')
+    print(name.text)
+    countries.append({"images": img, "name": name.text})
     urllib.request.urlretrieve(base_url+"image/"+img, img)
 
 with open("ja_countries.json", "w") as f:
--- src/App.tsx
+++ src/App.tsx
@@ -20,35 +20,39 @@ function App() {
   const images = importAll(require.context("./images", false, /\.(gif)$/));
   return (
     <div className="uk-text-muted">
-      <nav className="uk-navbar-container" data-uk-navbar>
-        <div className="uk-navbar-left">
-          <h1 className="uk-heading-small">picdex</h1>
+      <nav className="uk-container uk-container-expand">
+        <div className="uk-navbar">
+          <div className="uk-navbar-left">
+            <h1 className="uk-heading-small uk-logo">Picdex</h1>
+          </div>
         </div>
       </nav>
-      <ul
-        className="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s"
-        data-uk-sortable="handle: .uk-card"
-        data-uk-grid
-      >
-        {Countries.map((value) => {
-          return (
-            <div
-              className="uk-card uk-card-default uk-card-body uk-text-center"
-              key={value.images}
-            >
-              <input
-                type="image"
-                id="button"
-                src={images.get("./" + value.images)}
-                onClick={() => {
-                  Speak(value.name, "ja");
-                }}
-              ></input>
-              <div>{value.name}</div>
-            </div>
-          );
-        })}
-      </ul>
+      <div className="uk-container uk-container-expand">
+        <ul
+          className="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s"
+          data-uk-sortable="handle: .uk-card"
+          data-uk-grid
+        >
+          {Countries.map((value) => {
+            return (
+              <div
+                className="uk-card uk-card-default uk-card-body uk-text-center"
+                key={value.images}
+              >
+                <input
+                  type="image"
+                  id="button"
+                  src={images.get("./" + value.images)}
+                  onClick={() => {
+                    Speak(value.name, "ja");
+                  }}
+                ></input>
+                <div>{value.name}</div>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
