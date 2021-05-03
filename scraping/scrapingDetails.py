de66c77c95dd100a5b25cfb8d78d332bb80fae03
Yusaku Matsuki <ymatzki@gmail.com>
2021-05-03 18:13:20 +0900

    feat: add detail and english name

--- scraping/scrapingDetails.py
+++ scraping/scrapingDetails.py
@@ -5,25 +5,29 @@ from bs4 import BeautifulSoup
 
 
 base_url = 'https://www.mofa.go.jp/mofaj/kids/ichiran/'
-url = base_url + 'i_asia.html'
-# url = base_url + 'k_chuto.html'
-# url = base_url + 'k_n_america.html'
-# url = base_url + 'k_latinamerica.html'
-# url = base_url + 'k_europe.html's
-# url = base_url + 'k_oceania.html'
-# url = base_url + 'k_africa.html'
+# url = base_url + 'i_asia.html'
+# url = base_url + 'i_chuto.html'
+# url = base_url + 'i_n_america.html'
+# url = base_url + 'i_latinamerica.html'
+# url = base_url + 'i_europe.html'
+# url = base_url + 'i_oceania.html'
+url = base_url + 'i_africa.html'
 
 soup = BeautifulSoup(requests.get(url).content, "html.parser")
 
-countries = []
+details = []
+keys = ['name', 'en', 'capital', 'independent',
+        'language', 'area', 'population', 'currency']
 
 for tr in soup.find_all("tr"):
-    tr.children
-    print(tr.children)
-    # for td in tr.find_all("td"):
-    #     # countries.append({"name": td., "name": name.text})
-
-    #     print(td.get_text())
+    td = tr.find_all("td")
+    detail = dict()
+    index = 0
+    for td in tr.find_all("td"):
+        detail[keys[index]] = td.get_text()
+        index = index + 1
+    if bool(detail):
+        details.append(detail)
 
 with open("countries_ditails.json", "w") as f:
-    f.write(str(countries))
+    f.write(json.dumps(details, ensure_ascii=False, indent=2))
